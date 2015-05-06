1772f133dd05131fbb1cdb0ab82c739ba726bd75
spaceman1993 <roese.rene@gmx.de>
2015-05-06 17:14:36 +0200

    1.Design und Hinzufügen einer dynamischen Eingabe der angezeigten Ligen. Veränderbar unter Settings/Ligen

--- public/javascripts/mitStuProApp.js
+++ public/javascripts/mitStuProApp.js
@@ -1,6 +1,13 @@
 var app = angular.module('mitStuPro', ['ui.router', 'ui.bootstrap', 'colorpicker.module']);
 
 
+app.service('dataService', function() {
+	  // private variable
+	  var _activeLeagues = new Array();
+	  
+	  this.activeLeagues = _activeLeagues;
+	})
+
 
 app.factory('teams', [function(){
 	// ganze factory unwichtig. war nur für die Simulation der Punktetabelle
@@ -48,8 +55,12 @@ app.controller('MainCtrl', ['$scope', '$filter', 'teams', function($scope, $filt
 	};
 }]);
 
-app.controller("TableController", ['$scope', '$http', 'teams', function($scope, $http, teams) {
-
+app.controller("TableController", ['$scope', '$http', '$filter', 'dataService', 'teams', function($scope, $http, $filter, dataService, teams) {
+	
+	$scope.activeLeagues = dataService.activeLeagues;
+	
+	
+	
     $scope.items = [
 		{"itemId":1, "title":"Herren", "description":"Teams der Herren", 
 			"teams":[
@@ -117,22 +128,25 @@ app.controller("TableController", ['$scope', '$http', 'teams', function($scope, 
     
     $scope.chooseColor = function (input) {
     	
-    	if(input == 'Herren'){
+    	if(input == 'Herren' || input == 'Männlich'){
     		return 'btn-primary';
     	}
-    	else if(input == 'Damen'){
+    	else if(input == 'Damen' || input == 'Weiblich'){
     		return 'btn-danger';
     	}
     	
     };
   
-	$scope.getTableForLeague = function(adresse) {
+	$scope.getTableForLeague = function(league) {
         // Get data of team and show it in table
 		/*
 		 Durch das Two-Way-Databinding wird die HTML-Tabelle direkt geändert, wenn sich das Teams-Objekt ändert.
 		 Hier wird ein Name übergeben, über den die entsprechende Tabelle geholt wurde.
 		 An dieser Stelle muss nun natürlich die Tabelle extern geholt werden.
 		*/
+		$scope.leagueName = league.jugend + ' (' + league.name + ')';
+		adresse = league.linkage;
+		
     console.log(adresse);
 		teams.activeTeams = adresse;
     adresse = adresse.replace('/','%2F').replace('?','%3F').replace('=','%3D').replace('+','%2B').replace('&','%26').replace('+','%2B');
@@ -210,63 +224,66 @@ app.controller("TableController", ['$scope', '$http', 'teams', function($scope, 
     
 }]);
 
-app.controller('SettingsCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter){
-	  $scope.getAllLeagues = function() {
-		  var jsonFeed = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fbremerhv-handball.liga.nu%2Fcgi-bin%2FWebObjects%2FnuLigaHBDE.woa%2Fwa%2FleaguePage%3Fchampionship%3DBremer%2BHV%2B14%2F15%22%20and%20xpath%3D%22%2F%2Ftable%2F%2Ftr%2F%2Ftd%2F%2Ful%2F%2Fli%2F%2Fspan%2F%2Fa%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-		  // Actually fetch the data
-		    $http.get(jsonFeed).success(function (data) {
-		      // Define the unique market cities
-		      
-		      var leagues = new Array();
+app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', function($scope, $http, $filter, dataService){
+	
+	$scope.getAllLeagues = function(ligenplanLink) {
+			
+		console.log(ligenplanLink);
+			
+		https://bremerhv-handball.liga.nu/cgi-bin/WebObjects/nuLigaHBDE.woa/wa/leaguePage?championship=Bremer+HV+14/15
+		ligenplanLink = ligenplanLink.replace('"', '%3A').replace('/','%2F').replace('?','%3F').replace('=','%3D').replace('+','%2B').replace('&','%26').replace('+','%2B');
+		
+		var jsonFeed = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22" + ligenplanLink + "%22%20and%20xpath%3D%22%2F%2Ftable%2F%2Ftr%2F%2Ftd%2F%2Ful%2F%2Fli%2F%2Fspan%2F%2Fa%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
-		      var daten = data.query.results.a;
-		      for(var i = 1; i < daten.length; i++){
-		        var leagueData = daten[i];
-		       
-		        var league = new Object();
-		        
-		        var namensnennung = leagueData.content;
-		        
-		        league.id = i;
-		        
-		       
-		        if(namensnennung.indexOf("MJ") !== -1){
-		        	league.gender = "Männlich";
-		        	league.jugend = "Männliche " + namensnennung.substr(namensnennung.indexOf("MJ")+2, 1) + "-Jugend";
-		        	namensnennung = namensnennung.replace(" MJ"+namensnennung.substr(namensnennung.indexOf("MJ")+2, 1), "");
-		        }
-		        else if(leagueData.content.indexOf("WJ") !== -1){
-		        	league.gender = "Weiblich";
-		        	league.jugend = "Weibliche " + namensnennung.substr(namensnennung.indexOf("WJ")+2, 1) + "-Jugend";
-		        	namensnennung = namensnennung.replace(" WJ"+namensnennung.substr(namensnennung.indexOf("WJ")+2, 1), "");
-		        }
-		        else if(namensnennung.indexOf(" M") !== -1){
-		        	league.gender = "Männlich";
-		        	league.jugend = "Herren";
-		        	namensnennung = namensnennung.replace(" M", "");
-		        }
-		        else if(namensnennung.indexOf(" F") !== -1){
-		        	league.gender = "Weiblich";
-		        	league.jugend = "Damen";
-		        	namensnennung = namensnennung.replace(" F", "");
-		        }
-		        else{
-		        	league.gender = "undefined";
-		        	league.jugend = "undefined";
-		        }
-		        
-		        league.name = namensnennung;
-		        league.linkage = "https://bremerhv-handball.liga.nu" + leagueData.href;
-		        league.isActiv = false;
-		       
-		        leagues.push(league);
-		      }
+		$http.get(jsonFeed).success(function (data) {
+			
+			var leagues = new Array();
 
-		      $scope.leagues = leagues;
-		      
-		      $scope.listGroupBy( leagues, 'jugend');
-		    });
-	  };
+			var daten = data.query.results.a;
+		    	for(var i = 1; i < daten.length; i++){
+		    		
+		    		var leagueData = daten[i];
+		    		var league = new Object();
+		    		var namensnennung = leagueData.content;
+		        
+		    		league.id = i;
+		        
+			        if(namensnennung.indexOf("MJ") !== -1){
+			        	league.gender = "Männlich";
+			        	league.jugend = "Männliche " + namensnennung.substr(namensnennung.indexOf("MJ")+2, 1) + "-Jugend";
+			        	namensnennung = namensnennung.replace(" MJ"+namensnennung.substr(namensnennung.indexOf("MJ")+2, 1), "");
+			        }
+			        else if(leagueData.content.indexOf("WJ") !== -1){
+			        	league.gender = "Weiblich";
+			        	league.jugend = "Weibliche " + namensnennung.substr(namensnennung.indexOf("WJ")+2, 1) + "-Jugend";
+			        	namensnennung = namensnennung.replace(" WJ"+namensnennung.substr(namensnennung.indexOf("WJ")+2, 1), "");
+			        }
+			        else if(namensnennung.indexOf(" M") !== -1){
+			        	league.gender = "Männlich";
+			        	league.jugend = "Herren";
+			        	namensnennung = namensnennung.replace(" M", "");
+			        }
+			        else if(namensnennung.indexOf(" F") !== -1){
+			        	league.gender = "Weiblich";
+			        	league.jugend = "Damen";
+			        	namensnennung = namensnennung.replace(" F", "");
+			        }
+			        else{
+			        	league.gender = "undefined";
+			        	league.jugend = "undefined";
+			        }
+			        
+			        league.name = namensnennung;
+			        league.linkage = "https://bremerhv-handball.liga.nu" + leagueData.href;
+			        league.isActiv = false;
+			       
+			        leagues.push(league);
+		    	}
+
+		    	$scope.leagues = leagues;
+		    	$scope.jugenden = $filter('listGroupBy')( leagues, 'jugend');
+		});
+	};
 	  
 	  $scope.changeActiv = function(league) {
 		  if(league.isActiv){
@@ -277,55 +294,49 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', function($scope, $
 		  }
 	  };
 	 
-
-      // I group the friends list on the given property.
-      $scope.listGroupBy = function( list, attribute ) {
-
-          // First, reset the groups.
-          $scope.groups = [];
-
-          // Now, sort the collection of friend on the
-          // grouping-property. This just makes it easier
-          // to split the collection.
-          //$filter('orderObjectBy')( $scope.leagues, attribute );
-
-          // I determine which group we are currently in.
-          var groupValue = "_INVALID_GROUP_VALUE_";
-
-          // As we loop over each friend, add it to the
-          // current group - we'll create a NEW group every
-          // time we come across a new attribute value.
-          for ( var i = 0 ; i < list.length ; i++ ) {
-
-              var data = list[ i ];
-
-              // Should we create a new group?
-              if ( data[ attribute ] !== groupValue ) {
-
-                  var group = {
-                      label: data[ attribute ],
-                      list: []
-                  };
-
-                  groupValue = group.label;
-
-                  $scope.groups.push( group );
-
-              }
-
-  
-              group.list.push( data );
-
-          }
-
-      };
-      
-      // I am the grouped collection. Each one of these
-      // will contain a sub-collection of friends.
-      $scope.groups = [];
-	  
+	  $scope.setActiveLeagues = function() {
+		  
+		  activeLeagues = new Array();
+		  
+		  for (var i=0; i<$scope.leagues.length; i++){
+			  league = $scope.leagues[i]
+			  
+			  if(league.isActiv){
+				  activeLeagues.push(league);
+			  }
+		  }
+		  
+		  dataService.activeLeagues = activeLeagues;
+	  }
 	  
 }]);
+
+
+app.filter('listGroupBy', function() {
+	return function(list, attribute) {
+		groups = [];
+
+        var groupValue = "_INVALID_GROUP_VALUE_";
+
+        for ( var i = 0 ; i < list.length ; i++ ) {
+            var data = list[ i ];
+
+            if ( data[ attribute ] !== groupValue ) {
+                var group = {
+                    label: data[ attribute ],
+                    list: []
+                };
+                groupValue = group.label;
+
+                groups.push( group );
+            }
+            group.list.push( data );
+        }
+        
+        return groups;
+	};
+});
+
 
 app.filter('orderObjectBy', function() {
   return function(items, field, reverse) {
@@ -340,6 +351,20 @@ app.filter('orderObjectBy', function() {
     return filtered;
   };
 });
+
+app.filter('getObjectsWhereFieldIs', function() {
+	  return function(items, field, value) {
+	        var result = new Array();
+	        
+	        for (var i=0; i<items.length; i++){
+	        	if(items[i].field.contains(value)){
+	        		result.push(items[i]);
+	        	}
+	        }
+	        
+	        return result;
+	    };
+	});
 
 app.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
 
