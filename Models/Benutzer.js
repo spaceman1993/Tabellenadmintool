dca50585e0ee1d9a7565cd5be8bb2b536f96154c
Kamikaze910 <jan.gnther@yahoo.de>
2015-05-15 12:37:45 +0200

    Passwort Check.

--- Models/Benutzer.js
+++ Models/Benutzer.js
@@ -4,10 +4,25 @@
 var mongoose = require('mongoose');
 
 var benutzerSchema = mongoose.Schema({
-	id: String,
-    name: String,
-    passwort: String,
-    einstellung: Array
+	id : String,
+	name : String,
+	passwort : String,
+	einstellung : Array
 });
+
+benutzerSchema.methods.check = function(name, passwort) {
+	this.Benutzer.find(function(err, benutzers) {
+		if (err) {
+			return console.error(err);
+		}
+		for ( var benutzer in benutzers) {
+			if (benutzer.name === name) {
+				if (benutzer.passwort === passwort) {
+					// TODO Login
+				}
+			}
+		}
+	});
+}
 
 mongoose.model('Passwort', benutzerSchema);
