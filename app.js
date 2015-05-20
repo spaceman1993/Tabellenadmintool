07b55baca02d87e45c17e1a5d3f1f1e9a13d41fa
Kamikaze910 <jan.gnther@yahoo.de>
2015-05-20 14:52:23 +0200

    Module und routes angepastt.

--- app.js
+++ app.js
@@ -1,45 +1,39 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Schule');
+
+require('./models/Benutzer');
+
+var routes = require('./routes/index');
+var user = require('./routes/user');
 
 var app = express();
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/Schule');
-require('./models/Benutzer');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
-
+	console.log("Datenbank verbindung erfolgt!");
 });
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', routes);
+app.get('/users', user);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+module.exports = app;
