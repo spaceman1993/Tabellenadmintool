33fa61a18d67f1c6709f202e9aa691e99b18b5d2
MichaelW94 <michael.weber94@googlemail.com>
2015-05-21 10:36:40 +0200

    socket.io

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var express = require('express');
 var path = require('path');
 var http = require('http');
 var mongoose = require('mongoose');
+var io = require('socket.io');
 
 mongoose.connect('mongodb://localhost/Schule');
 
@@ -19,7 +20,7 @@ var app = express();
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
-	console.log("Datenbank verbindung erfolgt!");
+	console.log("Datenbankverbindung erfolgt!");
 });
 
 // all environments
@@ -27,13 +28,35 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var server = http.createServer(app);
+
+var io = io.listen(server);
+
+io.sockets.on('connection', function(socket) {
+	socket.on('createNote', function(data) {
+		socket.broadcast.emit('onNoteCreated', data);
+	});
+
+	socket.on('updateNote', function(data) {
+		socket.broadcast.emit('onNoteUpdated', data);
+	});
+
+	socket.on('moveNote', function(data){
+		socket.broadcast.emit('onNoteMoved', data);
+	});
+
+	socket.on('deleteNote', function(data){
+		socket.broadcast.emit('onNoteDeleted', data);
+	});
+});
+
+server.listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
 });
 
 module.exports = app;
