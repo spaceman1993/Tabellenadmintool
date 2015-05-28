ec9b22d84c8f4edf0e57ff976e07066c906f265b
spaceman1993 <roese.rene@gmx.de>
2015-05-28 16:40:33 +0200

    Umstellung Design-Manager / Entfernen unnötiger Funktionen / Sortierung der Funktionen

--- public/javascripts/mitStuProApp.js
+++ public/javascripts/mitStuProApp.js
@@ -171,12 +171,7 @@ app.controller("TableController", ['$scope', '$http', '$filter', 'dataService', 
     };
   
 	$scope.getTableForLeague = function(league) {
-        // Get data of team and show it in table
-		/*
-		 Durch das Two-Way-Databinding wird die HTML-Tabelle direkt geändert, wenn sich das Teams-Objekt ändert.
-		 Hier wird ein Name übergeben, über den die entsprechende Tabelle geholt wurde.
-		 An dieser Stelle muss nun natürlich die Tabelle extern geholt werden.
-		*/
+
 		$scope.mannschaften = null;
 		$scope.nextGames = null;
 		$scope.loading = true;
@@ -272,7 +267,7 @@ app.controller("TableController", ['$scope', '$http', '$filter', 'dataService', 
 	
 }]);
 
-app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'activUser', function($scope, $http, $filter, dataService, activUser){
+app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'activUser', 'ASCIIConverterService', function($scope, $http, $filter, dataService, activUser, ASCIIConverterService){
 	
 	$scope.beispielTabelle = [
 	                          { rang: 1, mannschaft: "THW Kiel", begegnungen: 33, siege: 29, unentschieden: 1, niederlagen: 3, tore: "1010:776", verhaeltnis: 234, punkte: "59:7" },
@@ -286,6 +281,8 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 	                          { rang: 9, mannschaft: "HSV Handball", begegnungen: 34, siege: 15, unentschieden: 2, niederlagen: 17, tore: "934:930", verhaeltnis: 4, punkte: "32:36" }
 	                        ];
 	
+	$scope.ligenplanLink = "https://bremerhv-handball.liga.nu/cgi-bin/WebObjects/nuLigaHBDE.woa/wa/leaguePage?championship=Bremer+HV+14/15";
+	
 	var isEmpty = function (obj) {
 	    for(var key in obj) {
 	        if(obj.hasOwnProperty(key))
@@ -294,48 +291,26 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 	    return true;
 	};
 	
-	$scope.initTableDesigns = function(){
-		$scope.tableDesign1 = new Object();
-		$scope.tableDesign1.backgroundColorHeader = "#030094";
-		$scope.tableDesign1.textColorHeader = "#ffffff";
-		$scope.tableDesign1.backgroundColor = "#ffffff";
-		$scope.tableDesign1.textColor = "#000000";
-		$scope.tableDesign1.tableStriped = true;
-		$scope.tableDesign1.stripedColor = "#cccccc";
-		$scope.tableDesign1.captionColor ="#000000";
-		$scope.tableDesign1.highlightColor = "#9ca0ff"
-		$scope.tableDesign1.highlightColor = "#9ca0ff"
-		$scope.tableDesign1.tableBordered = false;
-	};
-	
-	$scope.changeTableDesign = function(design){
-		$scope.tableDesign.backgroundColorHeader = design.backgroundColorHeader;
-		$scope.tableDesign.textColorHeader = design.textColorHeader;
-		$scope.tableDesign.backgroundColor = design.backgroundColor;
-		$scope.tableDesign.textColor = design.textColor;
-		$scope.tableDesign.tableStriped = design.tableStriped;
-		$scope.tableDesign.stripedColor = design.stripedColor;
-		$scope.tableDesign.captionColor = design.captionColor;
-		$scope.tableDesign.highlightColor = design.highlightColor;
-		$scope.tableDesign.tableBordered = design.tableBordered;
-	};
-	
-	$scope.initSettingsControllerVars = function() {
+	/**
+	 * Initialisierung des Settings-Controllers
+	 */
+	$scope.initSettingsController = function() {
 		$scope.benutzer = activUser.user;
-		$scope.tableDesign = dataService.tableDesign;
+		$scope.show = 'anzeigenManager';
 		
-		$scope.initTableDesigns();
-		
-		if(isEmpty($scope.tableDesign)){
-			$scope.changeTableDesign($scope.tableDesign1);
-		}
+		initDesignManager();
+		initLigaManager();
 	};
 	
-	$scope.showDeleteModal = function(league){
-		$scope.deleteLeague = league;
-		$scope.showDelete = true;
-	}
 	
+	
+	//------------------------------------------------------------------------------------------------------------------
+	//-----FUNKTIONEN FÜR DEN ANZEIGEN-MANAGER--------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Ändert den Status der aktiven Designs vom Benutzer
+	 */
 	$scope.changeDesignauswahl = function(designauswahl){
 		if(designauswahl.first){
 			$scope.benutzer.firstdesign = true;
@@ -352,6 +327,20 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 		}
 	};
 	
+	
+	/**
+	 * Ändert das Standarddesign vom Benutzer
+	 */
+	$scope.changeStandardDesign = function(standardDesign){
+		
+		$scope.benutzer.standardDesign = standardDesign;
+		
+	};
+	
+	
+	/**
+	 * Ändert ggf. das Standarddesign vom Benutzer bei Änderung der aktiven Designs
+	 */
 	$scope.changeFirstDesign = function() {
 		if($scope.benutzer.first){
 			$scope.benutzer.standardDesign = "1";
@@ -359,7 +348,11 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 			$scope.benutzer.standardDesign = "2";
 		}
 	};
+
 	
+	/**
+	 * Ändert ggf. das Standarddesign vom Benutzer bei Änderung der aktiven Designs
+	 */
 	$scope.changeSecondDesign = function() {
 		if($scope.benutzer.second){
 			$scope.benutzer.standardDesign = "2";
@@ -368,30 +361,111 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 		}
 	};
     
-	$scope.changeStandardDesign = function(standardDesign){
-		
-		$scope.benutzer.standardDesign = standardDesign;
-		
-	};
-	
+
+	/**
+	 * Ändert den Status des favorisierten Vereins vom Benutzer
+	 */
 	$scope.changeFavorisierterVerein = function(vereinsname){
 		$scope.benutzer.favoritVerein = vereinsname;
 	};
 	
+	
+	/**
+	 * Ändert den Status des Spielplans vom Benutzer
+	 */
 	$scope.changeSpielplan = function(spielplan){
 
 		$scope.benutzer.spielplan = spielplan;
 
 	};
 	
+	
+	/**
+	 * Ändert den Status der Loginmöglichkeit vom Benutzer
+	 */
 	$scope.changeLogin = function(verwaltung){
 
 		$scope.benutzer.verwaltung = verwaltung;
 			
 	};
 	
-	//Liga-Manager Init-Var
-	$scope.initLigaManagerVars = function() {
+	
+	
+	//------------------------------------------------------------------------------------------------------------------
+	//-----FUNKTIONEN FÜR DEN DESIGN-MANAGER----------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Initialisiert den Design-Manager
+	 */
+	var initDesignManager = function(){
+		//Vordefinierte Designs werden initialisiert
+		initTableDesigns();
+		
+		//Übernahme des Designs vom Benutzer
+		$scope.tableDesign = dataService.tableDesign;
+		
+		//Sollte kein Design vorhanden sein wird das erste Design als Standard-Design eingerichtet
+		if(isEmpty($scope.tableDesign)){
+			$scope.changeTableDesign($scope.tableDesign1);
+		}
+	};
+	
+	
+	/**
+	 * Initialiserung der Standarddesigns des Programms
+	 */
+	var initTableDesigns = function(){
+		$scope.tableDesign1 = createTableDesign("#030094", "#ffffff", "#ffffff", "#000000", true, "#cccccc", "#000000", "#9ca0ff", false);
+		$scope.tableDesign2 = createTableDesign("#030094", "#ffffff", "#ffffff", "#000000", true, "#cccccc", "#000000", "#9ca0ff", false);
+		$scope.tableDesign3 = createTableDesign("#030094", "#ffffff", "#ffffff", "#000000", true, "#cccccc", "#000000", "#9ca0ff", false);
+	};
+	
+	
+	/**
+	 * Funktion liefert ein gefertigtes Design zurück
+	 */
+	var createTableDesign = function(backgroundColorHeader, textColorHeader, backgroundColor, textColor, tableStriped, stripedColor, captionColor, highlightColor, tableBordered){
+		var tableDesign = new Object();
+		tableDesign.backgroundColorHeader = backgroundColorHeader;
+		tableDesign.textColorHeader = textColorHeader;
+		tableDesign.backgroundColor = backgroundColor;
+		tableDesign.textColor = textColor;
+		tableDesign.tableStriped = tableStriped;
+		tableDesign.stripedColor = stripedColor;
+		tableDesign.captionColor =captionColor;
+		tableDesign.highlightColor = highlightColor;
+		tableDesign.tableBordered = tableBordered;
+		
+		return tableDesign;
+	};
+	
+	
+	/**
+	 * Ändert das Design in ein anderes Design
+	 */
+	$scope.changeTableDesign = function(design){
+		$scope.tableDesign.backgroundColorHeader = design.backgroundColorHeader;
+		$scope.tableDesign.textColorHeader = design.textColorHeader;
+		$scope.tableDesign.backgroundColor = design.backgroundColor;
+		$scope.tableDesign.textColor = design.textColor;
+		$scope.tableDesign.tableStriped = design.tableStriped;
+		$scope.tableDesign.stripedColor = design.stripedColor;
+		$scope.tableDesign.captionColor = design.captionColor;
+		$scope.tableDesign.highlightColor = design.highlightColor;
+		$scope.tableDesign.tableBordered = design.tableBordered;
+	};
+	
+	
+	
+	//------------------------------------------------------------------------------------------------------------------
+	//-----FUNKTIONEN FÜR DEN LIGA-MANAGER------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Initialisiert Variablen für den Liga-Manager
+	 */
+	var initLigaManager = function() {
 		$scope.leagues = dataService.leagues;
 		$scope.jugenden = $filter('listGroupBy')( $scope.leagues, 'jugend');
 		
@@ -400,9 +474,15 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 		
 		$scope.favorit = $scope.benutzer.favorit;
 		
+		$scope.progressbaraktuell = 0;
+		$scope.progressbaraktuellprozent = 0;
 	};
 	
-	$scope.checkJugenden = function(data){
+	
+	/**
+	 * Überprüft ob Liga-Daten schon vorliegen und aktiviert bei Fund das PopUp-Fenster für den Update-Hinweis
+	 */
+	$scope.checkLigaData = function(data){
 		if(isEmpty(data)){
 			$scope.getAllLeagues($scope.ligenplanLink);
 		}
@@ -412,7 +492,14 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 	}
 	
 	
-	$scope.ligenplanLink = "https://bremerhv-handball.liga.nu/cgi-bin/WebObjects/nuLigaHBDE.woa/wa/leaguePage?championship=Bremer+HV+14/15";
+	/**
+	 * Ein PopUp-Fenster wird aktiviert mit dem Hinweis zum Löschen der Liga
+	 */
+	$scope.showDeleteModal = function(league){
+		$scope.deleteLeague = league;
+		$scope.showDelete = true;
+	}
+
 	
 	$scope.getAllLeagues = function(ligenplanLink) {
 
@@ -421,7 +508,7 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 		$scope.jugenden = null;
 		$scope.loading = true;
 		
-		ligenplanLink = ligenplanLink.replace('"', '%3A').replace('/','%2F').replace('?','%3F').replace('=','%3D').replace('+','%2B').replace('&','%26').replace('+','%2B');
+		ligenplanLink = ASCIIConverterService.convert(ligenplanLink);
 		
 		var jsonFeed = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22" + ligenplanLink + "%22%20and%20xpath%3D%22%2F%2Ftable%2F%2Ftr%2F%2Ftd%2F%2Ful%2F%2Fli%2F%2Fspan%2F%2Fa%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
@@ -476,36 +563,45 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 		    	$scope.loading = false;
 		    	
 		    	dataService.leagues = leagues;
-		    	$scope.initLigaManagerVars();
+		    	initLigaManager();
 		    	
 		});
 	};
 	
 	
+	/**
+	 * Ändert den Status der favorisierten Liga vom Benutzer
+	 */
 	$scope.changeFavorit = function(league){
 		$scope.benutzer.favoritLeague = league;
 	};
 	
+	
+	/**
+	 * Überprüft wie sich der Suche-Button verhalten muss und aktiviert die dazugehörige Funktion
+	 */
 	$scope.checkSearchVerein = function(){
-		
+		//Aktiviert das Suchfeld für den Eintrag
 		if(!$scope.search && !$scope.searchStart){
 			$scope.search = true;
 		}
+		//Eintrag ins Suchfeld ist erfolgt und Suche wird gestartet
 		else if($scope.searchVerein  && !$scope.searchStart){
 			$scope.search = false;
 			$scope.searchStart = true;
 			findAllLeaguesOfVerein($scope.searchVerein);
 		}
+		//Deaktivierung des Suchfelds
 		else{
 			$scope.search = false;
 		}
 	};
 	
-	$scope.progressbaraktuell = 0;
-	$scope.progressbaraktuellprozent = 0;
-	
+
+	/**
+	 * Überprüft sämtliche Ligen auf Vereinsstimmigkeit
+	 */
 	var findAllLeaguesOfVerein = function (verein){
-		
 		var leagues = $scope.leagues;
 		$scope.anzahlSearch = 0;
 		
@@ -514,6 +610,9 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 		}
 	};
 	
+	/**
+	 * Überprüft in der Liga, ob sich der Verein darin aufhält & aktiviert die Liga bei Fund
+	 */
 	var checkIfVereinInLeague = function(league, verein) {
 		
 		var adresse = league.linkage;
@@ -529,7 +628,6 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 	      try{
 		      var daten = data.query.results.table[0].tbody.tr;
 	
-		      
 		      for(var i = 1; i < daten.length; i++){
 		        var mannschaftsdaten = daten[i].td;
 		        var mannschaft = new Object();
@@ -567,16 +665,20 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 	    });
 	  };
 	
+	  
+	  /**
+	   * Ändert den Status der Liga ins Gegenteil von dem was ist vorher gewesen ist
+	   */
 	  $scope.changeActiv = function(league) {
 		  if(league.isActiv){
-			  $scope.deleteActivLeague(league);
+			  deleteActivLeague(league);
 		  }
 		  else{
-			  $scope.addActivLeague(league);
+			  addActivLeague(league);
 		  }
 	  };
 	  
-	  $scope.addActivLeague = function(league) {
+	  var addActivLeague = function(league) {
 		  league.isActiv = true;
 		  $scope.activeLeagues = $filter('filter')($scope.leagues, {isActiv: 'true'});
 		  $scope.activJugenden =  $filter('listGroupBy')( $scope.activeLeagues, 'jugend');
@@ -588,7 +690,7 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 		  dataService.leagues = $scope.leagues;
 	  };
 	  
-	  $scope.deleteActivLeague = function(league) {
+	  var deleteActivLeague = function(league) {
 		  league.isActiv = false;
 		  $scope.activeLeagues = $filter('filter')($scope.leagues, {isActiv: 'true'});
 		  $scope.activJugenden =  $filter('listGroupBy')( $scope.activeLeagues, 'jugend');
@@ -604,10 +706,22 @@ app.controller('SettingsCtrl', ['$scope', '$http', '$filter', 'dataService', 'ac
 		  league.specialName = specialName;
 	  };
 	  
+	  /**
+	   * Dient zum Kontrollieren des Controllers innerhalb einer Directive
+	   */
 	  $scope.focusinControl = {
 	  };
 	  
 }]);
+
+app.service('ASCIIConverterService', function(){
+    
+    this.convert = function(text) {
+    	var text = text.replace('/','%2F').replace('?','%3F').replace('=','%3D').replace('+','%2B').replace('&','%26').replace('+','%2B');
+    	return text; 
+    };
+
+});
 
 app.directive('ngEnter', function () {
     return function (scope, element, attrs) {
@@ -697,6 +811,21 @@ app.filter('listGroupBy', function() {
 });
 
 
+app.filter('orderObjectBy', function() {
+  return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) {filtered.reverse();}
+    return filtered;
+  };
+});
+
+
 app.directive('ligatabelle', function () {
     // return the directive definition object 
     return {
@@ -737,7 +866,7 @@ app.directive('spielplantabelle', function () {
         	nextgames: '=',
         	tabledesign: '='
         },
-        controller: function ($scope) {
+        controller: function ($scope, $http) {
             
         	$scope.modalShown = false;
         	
